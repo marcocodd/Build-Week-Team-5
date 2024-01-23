ebf7b58dc48ccdf84d59f1db9faad09f1036cb51
Antonio Ruggia Piquer <149959739+kaem0n@users.noreply.github.com>
2024-01-23 19:24:10 +0100

    code polish

--- BW2-Spotify-assets/assets/Js/greetings.js
+++ BW2-Spotify-assets/assets/Js/greetings.js
@@ -1,13 +1,13 @@
 const greetinsPage = function () {
- const hourOfTheDay = new Date().getHours();
- const greetingText = document.getElementById("greeting");
- if (hourOfTheDay >= 6 && hourOfTheDay < 12) {
-  greetingText.innerText = "Buongiorno";
- } else if (hourOfTheDay >= 12 && hourOfTheDay < 18) {
-  greetingText.innerText = "Buon pomeriggio";
- } else {
-  greetingText.innerText = "Buona sera";
- }
-};
+  const hourOfTheDay = new Date().getHours()
+  const greetingText = document.getElementById('greeting')
+  if (hourOfTheDay >= 6 && hourOfTheDay < 12) {
+    greetingText.innerText = 'Buongiorno'
+  } else if (hourOfTheDay >= 12 && hourOfTheDay < 18) {
+    greetingText.innerText = 'Buon pomeriggio'
+  } else {
+    greetingText.innerText = 'Buonasera'
+  }
+}
 
-greetinsPage();
+greetinsPage()
