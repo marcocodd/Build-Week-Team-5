a77694abc4887b605651409e9bc5880dca7d8e0c
Matteo Furgani <151537051+MatteoFurgani@users.noreply.github.com>
2024-01-25 11:33:40 +0100

    modifiche back-forward

--- BW2-Spotify-assets/assets/Js/mainAlbum.js
+++ BW2-Spotify-assets/assets/Js/mainAlbum.js
@@ -1,50 +1,50 @@
 // Funzione per la media dei colori
 
 function getAverageColor(imageUrl, callback) {
- let img = new Image();
- img.crossOrigin = "Anonymous";
- img.src = imageUrl;
+  let img = new Image();
+  img.crossOrigin = "Anonymous";
+  img.src = imageUrl;
 
- img.onload = function () {
-  let canvas = document.createElement("canvas");
-  let ctx = canvas.getContext("2d");
+  img.onload = function () {
+    let canvas = document.createElement("canvas");
+    let ctx = canvas.getContext("2d");
 
-  canvas.width = img.width;
-  canvas.height = img.height;
-  ctx.drawImage(img, 0, 0);
+    canvas.width = img.width;
+    canvas.height = img.height;
+    ctx.drawImage(img, 0, 0);
 
-  let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
+    let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
 
-  let sumRed = 0,
-   sumGreen = 0,
-   sumBlue = 0;
+    let sumRed = 0,
+      sumGreen = 0,
+      sumBlue = 0;
 
-  for (let i = 0; i < imageData.length; i += 4) {
-   sumRed += imageData[i];
-   sumGreen += imageData[i + 1];
-   sumBlue += imageData[i + 2];
-  }
+    for (let i = 0; i < imageData.length; i += 4) {
+      sumRed += imageData[i];
+      sumGreen += imageData[i + 1];
+      sumBlue += imageData[i + 2];
+    }
 
-  let pixelCount = imageData.length / 4;
-  let averageRed = sumRed / pixelCount;
-  let averageGreen = sumGreen / pixelCount;
-  let averageBlue = sumBlue / pixelCount;
+    let pixelCount = imageData.length / 4;
+    let averageRed = sumRed / pixelCount;
+    let averageGreen = sumGreen / pixelCount;
+    let averageBlue = sumBlue / pixelCount;
 
-  let averageColor = {
-   red: Math.round(averageRed),
-   green: Math.round(averageGreen),
-   blue: Math.round(averageBlue),
+    let averageColor = {
+      red: Math.round(averageRed),
+      green: Math.round(averageGreen),
+      blue: Math.round(averageBlue),
+    };
+
+    callback(averageColor);
   };
-
-  callback(averageColor);
- };
 }
 
 // Funzione per applicare lo sfondo
 
 function applyBackgroundColor(albumContainer, averageColor) {
- albumContainer.style.transition = "background-color 0.5s ease";
- albumContainer.style.backgroundColor = `rgb(${averageColor.red}, ${averageColor.green}, ${averageColor.blue})`;
+  albumContainer.style.transition = "background-color 0.5s ease";
+  albumContainer.style.backgroundColor = `rgb(${averageColor.red}, ${averageColor.green}, ${averageColor.blue})`;
 }
 
 // id e url per fetch
@@ -53,53 +53,53 @@ const idMainAlbum = urlMainAlbum.get("id");
 const urlMainFetch = "https://striveschool-api.herokuapp.com/api/deezer/album/";
 
 fetch(urlMainFetch + idMainAlbum)
- .then((response) => {
-  if (response.ok) {
-   return response.json();
-  } else {
-   throw new Error(response.status);
-  }
- })
- .then((albumData) => {
-  console.log(albumData);
-  // Funzione per ottenere gli artisti dal formato dell'API
-  const getContributors = function () {
-   let text = "";
-   for (let i = 0; i < albumData.contributors.length; i++) {
-    if (i === 0) {
-     text = `<a href="./Artist.html?id=${albumData.contributors[i].id}" class="text-capitalize text-white link-underline link-underline-opacity-0">${albumData.contributors[i].name}</a>`;
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
     } else {
-     text += `, <a href="./Artist.html?id=${albumData.contributors[i].id}" class="text-capitalize text-white link-underline link-underline-opacity-0">${albumData.contributors[i].name}</a>`;
+      throw new Error(response.status);
     }
-   }
-   return text;
-  };
+  })
+  .then((albumData) => {
+    console.log(albumData);
+    // Funzione per ottenere gli artisti dal formato dell'API
+    const getContributors = function () {
+      let text = "";
+      for (let i = 0; i < albumData.contributors.length; i++) {
+        if (i === 0) {
+          text = `<a href="./Artist.html?id=${albumData.contributors[i].id}" class="text-capitalize text-white link-underline link-underline-opacity-0">${albumData.contributors[i].name}</a>`;
+        } else {
+          text += `, <a href="./Artist.html?id=${albumData.contributors[i].id}" class="text-capitalize text-white link-underline link-underline-opacity-0">${albumData.contributors[i].name}</a>`;
+        }
+      }
+      return text;
+    };
 
-  // Funzione per ottenere i generi dal formato dell'API
-  const getGenres = function () {
-   let text = "";
-   if (albumData.genres.data.length === 0) {
-    text = `<a href="#" class="text-capitalize text-white link-underline link-underline-opacity-0">Altro</a>`;
-   } else {
-    for (let i = 0; i < albumData.genres.data.length; i++) {
-     if (i === 0) {
-      text = `<a href="#" class="text-capitalize text-white link-underline link-underline-opacity-0">${albumData.genres.data[i].name}</a>`;
-     } else {
-      text += `, <a href="#" class="text-capitalize text-white link-underline link-underline-opacity-0">${albumData.genres.data[i].name}</a>`;
-     }
-    }
-   }
-   return text;
-  };
+    // Funzione per ottenere i generi dal formato dell'API
+    const getGenres = function () {
+      let text = "";
+      if (albumData.genres.data.length === 0) {
+        text = `<a href="#" class="text-capitalize text-white link-underline link-underline-opacity-0">Altro</a>`;
+      } else {
+        for (let i = 0; i < albumData.genres.data.length; i++) {
+          if (i === 0) {
+            text = `<a href="#" class="text-capitalize text-white link-underline link-underline-opacity-0">${albumData.genres.data[i].name}</a>`;
+          } else {
+            text += `, <a href="#" class="text-capitalize text-white link-underline link-underline-opacity-0">${albumData.genres.data[i].name}</a>`;
+          }
+        }
+      }
+      return text;
+    };
 
-  // Funzione per calcolare la durata totale in minuti
-  const calcolaMinutiTotali = function (tracks) {
-   let totalDuration = 0;
-   tracks.forEach((track) => {
-    totalDuration += track.duration;
-   });
-   return Math.floor(totalDuration / 60);
-  };
+    // Funzione per calcolare la durata totale in minuti
+    const calcolaMinutiTotali = function (tracks) {
+      let totalDuration = 0;
+      tracks.forEach((track) => {
+        totalDuration += track.duration;
+      });
+      return Math.floor(totalDuration / 60);
+    };
 
     // Popolare dinamicamente la pagina ALBUM
     const albumContainer = document.getElementById("main-card");
@@ -107,8 +107,8 @@ fetch(urlMainFetch + idMainAlbum)
     <div class="col-8 col-lg-3 py-4 text-center align-self-center mx-auto mx-lg-0">
     <a href="./Album.html?id=${albumData.id}">
       <img src="${albumData.cover_xl}" alt="${
-   albumData.md5_image
-  }" class="w-100">
+      albumData.md5_image
+    }" class="w-100">
     </a>
   </div>
   <div class="col-lg-9 py-3 d-flex flex-column justify-content-center align-items-start">
@@ -117,21 +117,21 @@ fetch(urlMainFetch + idMainAlbum)
     </div>
     <h1 class="fw-bold">
       <a href="./Album.html?id=${
-       albumData.id
+        albumData.id
       }" class="text-white link-underline link-underline-opacity-0">${
-   albumData.title
-  }</a>
+      albumData.title
+    }</a>
     </h1>
     <div>
       <img src="${
-       albumData.artist.picture_small
+        albumData.artist.picture_small
       }" alt="Foto Gruppo" class="rounded-circle" width="30" height="30">
       <p class="badge text-white text-white">
         <span>${getContributors()}</span> &middot;
         <span>${albumData.release_date.slice(0, 4)}</span> &middot;
         <span>${albumData.nb_tracks} brani,</span>
         <span class="opacity-50 ms-2">${calcolaMinutiTotali(
-         albumData.tracks.data
+          albumData.tracks.data
         )} min</span>
       </p>
     </div>
@@ -139,23 +139,33 @@ fetch(urlMainFetch + idMainAlbum)
 
     `;
 
-  const albumImage = document.getElementById("albumImage");
+    const albumImage = document.getElementById("albumImage");
 
-  // Chiamata alla funzione per ottenere la media dei colori e applicare lo sfondo al main
-  getAverageColor(albumData.cover_xl, function (averageColor) {
-   // Applica lo sfondo al main
-   const mainElement = document.getElementById("background-avg");
-   applyBackgroundColor(mainElement, averageColor);
+    // Chiamata alla funzione per ottenere la media dei colori e applicare lo sfondo al main
+    getAverageColor(albumData.cover_xl, function (averageColor) {
+      // Applica lo sfondo al main
+      const mainElement = document.getElementById("background-avg");
+      applyBackgroundColor(mainElement, averageColor);
+    });
+
+    // // Chiamata alla funzione per ottenere la media dei colori e applicare lo sfondo
+    // getAverageColor(albumData.cover_xl, function (averageColor) {
+    //  applyBackgroundColor(albumContainer, averageColor);
+    // });
+  })
+  .catch((error) => {
+    console.log(error);
   });
 
-  // // Chiamata alla funzione per ottenere la media dei colori e applicare lo sfondo
-  // getAverageColor(albumData.cover_xl, function (averageColor) {
-  //  applyBackgroundColor(albumContainer, averageColor);
-  // });
- })
- .catch((error) => {
-  console.log(error);
- });
+document.getElementById("go-back").addEventListener("click", () => {
+  console.log("indietro");
+  history.back(); // Torna alla pagina precedente
+});
+
+document.getElementById("go-forward").addEventListener("click", () => {
+  console.log("avanti");
+  history.forward(); // Ricarica la pagina corrente
+});
 
 // //////////////////////////////CODICE FUNZIONANTE
 
